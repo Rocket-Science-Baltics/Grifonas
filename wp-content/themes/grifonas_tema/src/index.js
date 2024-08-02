6d1b1f7d47a6153840219132a03dd5889cf892b1
rocketsciencelt <mantas@rocketscience.lt>
2024-08-02 09:45:39 +0300

    Deleted wrong theme.

--- wp-content/themes/grifonas_tema/src/index.js
+++
@@ -1,8 +0,0 @@
-import './scss/index.scss';
-import initBase from "./js/initBase";
-import initComponents from "./js/components";
-
-window.onload = function () {
-    initComponents();
-    initBase();
-}
