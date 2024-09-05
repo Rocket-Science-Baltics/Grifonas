1bafe6530ddf8f5c298d4bb4df4fcebbb726ec49
rocketsciencelt <mantas@rocketscience.lt>
2024-09-05 11:30:38 +0300

    Added made by RS.

--- wp-content/themes/grifonas/footer.php
+++ wp-content/themes/grifonas/footer.php
@@ -6,8 +6,9 @@ $privacyPage = get_field('content_right', 'options');
     </div>
     <footer class="bg-aliceBlueGR border-t border-blueGR py-5">
         <div class="container">
-            <div class="flex justify-between items-center w-full px-2.5">
+            <div class="flex justify-between max-sm:flex-col items-center w-full px-2.5">
                 <div class="text-brownGR"><?php echo $copyright; ?></div>
+                <div class="max-sm:order-3"><a href="https://rocketscience.lt" class="text-brownGR">Made by Rocket Science Baltics</a></div>
                 <div><a href="<?php echo $privacyPage; ?>" class="text-brownGR"><?php echo __('Privatumo politika', 'theme'); ?></a></div>
             </div>
         </div>
