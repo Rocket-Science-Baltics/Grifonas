c179eebc36d8f9d8c984beb8c50fcaf6d96fa26a
rocketsciencelt <mantas@rocketscience.lt>
2024-09-16 13:23:56 +0300

    Added translation for rs dev copyright.

--- wp-content/themes/grifonas/footer.php
+++ wp-content/themes/grifonas/footer.php
@@ -8,7 +8,7 @@ $privacyPage = get_field('content_right', 'options');
         <div class="container">
             <div class="flex justify-between max-sm:flex-col items-center w-full px-2.5">
                 <div class="text-brownGR"><?php echo $copyright; ?></div>
-                <div class="max-sm:order-3"><a href="https://rocketscience.lt" class="text-brownGR">Made by Rocket Science Baltics</a></div>
+                <div class="max-sm:order-3"><a href="https://rocketscience.lt" class="text-brownGR"><?php pll_e('Svetainę sukūrė: Rocket Science Baltics') ?></a></div>
                 <div><a href="<?php echo $privacyPage; ?>" class="text-brownGR"><?php pll_e('Privatumo politika'); ?></a></div>
             </div>
         </div>
--- wp-content/themes/grifonas/functions/lang.php
+++ wp-content/themes/grifonas/functions/lang.php
@@ -1,4 +1,5 @@
 <?php
 add_action('init', function() {
-    pll_register_string('mytheme-hello', 'Privatumo politika');
+    pll_register_string('grifonas-theme', 'Privatumo politika');
+    pll_register_string('grifonas-theme', 'Svetainę sukūrė: Rocket Science Baltics');
 });
